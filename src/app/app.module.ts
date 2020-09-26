aee2b6df16c7757c1bb4a0b42e6ea9b5073decce
lukasz <lukasz.stojecki@o2.pl>
2020-09-26 10:50:39 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+import { StudentEditComponent } from './student-edit/student-edit.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { StudentNewComponent } from './student-new/student-new.component';
+import { StudentSearchComponent } from './student-search/student-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent,
+    StudentEditComponent,
+    NotFoundComponent,
+    StudentNewComponent,
+    StudentSearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/students/students.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Student } from '../student';
+import { StudentService } from '../student.service';
+
+@Component({
+  selector: 'app-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.css']
+})
+export class StudentsComponent implements OnInit {
+
+  students: Student[];
+
+  constructor(private studentService: StudentService) { }
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents(): void {
+    this.studentService.getStudents()
+      .subscribe(students => this.students = students);
+  }
+
+  delete(student: Student): void {
+    let result = window.confirm("Czy na pewno chcesz usunąć studenta?")
+
+    if(result){
+      this.students = this.students.filter(s => s !== student);
+      this.studentService.deleteStudent(student).subscribe();
+    }
+
+  }
+
+}
